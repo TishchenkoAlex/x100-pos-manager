fb094f4af2f1173d5eec672a4e29b138bbdb564a
Tischenko Alexander <tischenko.a@gmail.com>
2020-01-26 18:38:02 +0200

    Добавьте файлы проекта.

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace x100_pos_manager.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
